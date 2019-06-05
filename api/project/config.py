046f980255b6c929282fbef44142790330ab4c9e
kaoss <swojak.a@gmail.com>
2019-06-05 22:32:31 +0200

    config fix: add default uris for db if not started in docker

--- api/project/config.py
+++ api/project/config.py
@@ -8,6 +8,7 @@ class BaseConfig:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
@@ -19,9 +20,9 @@ class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL', 'postgres://postgres:postgres@0.0.0.0:5432/users_tests')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@0.0.0.0:5432/users_prod')
