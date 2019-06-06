e59883a79623254d60130396beba84ce48491035
kaoss <swojak.a@gmail.com>
2019-06-06 11:06:36 +0200

    fix manage.py: uncomment drop_all in recreate_db

--- api/manage.py
+++ api/manage.py
@@ -14,7 +14,7 @@ manager.add_command("runserver", Server(host="0.0.0.0", port=3300))
 @manager.command
 def recreate_db():
     """Recreates a database"""
-    # db.drop_all()
+    db.drop_all()
     db.create_all()
     db.session.commit()
 
